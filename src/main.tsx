2777da45dd26de218e72ed83eb38af34df5cfc9a
Hancharou <pl.hancharou@gmail.com>
2025-03-18 10:39:59 +0100

    feat: implement react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 import App from './App.tsx';
 import { ErrorBoundary, Fallback } from './components';
 import './index.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <ErrorBoundary FallbackComponent={Fallback}>
-      <App />
-    </ErrorBoundary>
+    <BrowserRouter basename="/">
+      <ErrorBoundary FallbackComponent={Fallback}>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
   </StrictMode>
 );
